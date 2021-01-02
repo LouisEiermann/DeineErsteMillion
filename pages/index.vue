372635ee83adc47dac5352f107d5fff36b34070d
LouisEiermann <louis.eiermann@gmail.com>
2021-01-02 14:16:29 +0100

    added css

--- pages/index.vue
+++ pages/index.vue
@@ -4,13 +4,11 @@
       Beantworte die Fragen, um herauszufinden, wann Du deine erste Million
       erreicht haben wirst...
     </div>
-    <div class="flex-item parameters">
-      <form>
-        <input class="input" placeholder="Startkapital in Euro" v-model="kapital" />
-        <input class="input" placeholder="Sparrate in Euro p. M." v-model="sparrate" />
-        <input class="input" placeholder="Zinssatz in Prozent" v-model="investment" />
-      </form>
-      <button @click="calculate">Rechnen</button>
+    <div class="flex-item parameters flex-container">
+      <input class="input" placeholder="Startkapital in Euro" v-model="kapital" />
+      <input class="input" placeholder="Sparrate in Euro p. M." v-model="sparrate" />
+      <input class="input" placeholder="Zinssatz in Prozent" v-model="investment" />
+      <button class="btn submit" @click="calculate">Rechnen</button>
     </div>
     <div class="flex-item result">
       Du bist Millionär in {{ jahre }} Jahren !
@@ -25,7 +23,7 @@ import Calculator from "@/repository/Calculator.js"
 export default Vue.extend({
   data() {
     return {
-      jahre: null,
+      jahre: "...",
       kapital: null,
       sparrate: null,
       investment: null
@@ -53,16 +51,24 @@ export default Vue.extend({
 .headline {
   font-size: 2rem;
   margin-top: 5rem;
+  color: #fff;
 }
 
 .input {
-    padding: .7rem;
-    margin: 2rem;
-    display: block;
+  font-size: 2rem;
+  padding: .7rem;
+  margin: 1rem;
+  border: .1rem solid #fff;
+  border-radius: 1rem;
+}
+
+.submit {
+  margin-top: 1rem;
 }
 
 .result {
   font-size: 4rem;
-  margin-top: 20rem;
+  margin-top: 10rem;
+  color: #fff;
 }
 </style>
